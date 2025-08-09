66e4e26d5879b5c79dd999b15d9dae3a51590659
SubhanAlom009 <subhanalombasic123@gmail.com>
2025-08-09 17:42:14 +0530

    fixed some issues

--- app/auth/signup/page.jsx
+++ app/auth/signup/page.jsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useState } from "react";
-import { redirect, useRouter } from "next/navigation";
+import { useRouter } from "next/navigation";
 import { signIn } from "next-auth/react";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
